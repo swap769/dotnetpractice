300de47c6d18f470af82692df547ad93693047b8
harshsrivastav <91178048+srivastavharsh@users.noreply.github.com>
2023-09-30 22:02:36 +0530

    Update ErrorViewModel.cs

--- ErrorViewModel.cs
+++ ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace website.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public boolean? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
